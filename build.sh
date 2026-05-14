593d0c95cb908ce9abc05212bb1892659a320304
randomdude-67 <leishiyuan@outlook.com>
2026-05-13 23:03:56 -0400

    Credit Claude for the port, support public repo download

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ curl -L -o Mindustry.jar \
   https://github.com/Anuken/Mindustry/releases/download/v157.4/Mindustry.jar
 
 echo "--- Downloading pre-built JRE ext2 image ---"
-curl -L -o jre.ext2 \
+curl -L ${GITHUB_TOKEN:+-H "Authorization: token $GITHUB_TOKEN"} -o jre.ext2 \
   https://github.com/randomdude-67/mindustryport/releases/download/jre-v17/jre.ext2
 echo "jre.ext2 size: $(du -sh jre.ext2)"
 
